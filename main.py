d47712543f8f3b22fb411ed75691478a49fbb260
maayan89 <maayan089@gmail.com>
2025-01-26 15:19:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+from image_utils.py import load_image, edge_detection
+from skimage.filters import median
+from skimage.morphology import ball
 
+dogs = load_image('dogs.jpg')
+clean_image = median(dogs, ball(3))
